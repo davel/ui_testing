12d9043ab76c590b09249f3859e54465096c0bdb
ponch <ponch78@gmail.com>
2013-02-23 14:13:43 +0000

    Use anchor instead of <ENTER>

--- lib/pages/jobs_home_page.rb
+++ lib/pages/jobs_home_page.rb
@@ -5,10 +5,11 @@ class JobsHomePage
   expected_title "Jobs in London, the UK &amp; Beyond | Adzuna"
 
   text_field :search_box, :id => 'w'
+  a :search, :class => 'subm'
 
   def search_for search_term
     self.search_box = search_term
-    @browser.send_keys('{ENTER}')
+    search
   end
 
 end
